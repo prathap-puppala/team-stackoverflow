fdd47d7390b2991c75476f7af242e3934b54302a
Prathap Puppala <prathappuppala13@gmail.com>
2019-01-21 12:43:57 +0530

    Code after merging

--- db/migrate/20190121062727_remove_up_vote_down_vote_score_from_questions.rb
+++ db/migrate/20190121062727_remove_up_vote_down_vote_score_from_questions.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
 
+# This is Migration file
 class RemoveUpVoteDownVoteScoreFromQuestions < ActiveRecord::Migration[5.2]
   def change
     remove_column :questions, :ans_upvote_score
